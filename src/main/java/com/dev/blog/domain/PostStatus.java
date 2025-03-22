1b9b0910046c35e5924434dfb9ef5897dfee0a25
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-03-22 10:49:55 +0530

    basic setup(lombok & map construct) and entity relationships

---
+++ src/main/java/com/dev/blog/domain/PostStatus.java
@@ -0,0 +1,6 @@
+package com.dev.blog.domain;
+
+public enum PostStatus {
+    DRAFT,
+    PUBLISHED
+}
---
+++ src/main/java/com/dev/blog/domain/entities/Post.java
@@ -0,0 +1,84 @@
+package com.dev.blog.domain.entities;
+
+import com.dev.blog.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id",nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(name = "posts_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new HashSet<>();
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals( id, post.id ) && Objects.equals( title, post.title ) && Objects.equals( content, post.content ) && status == post.status && Objects.equals( readingTime, post.readingTime ) && Objects.equals( createdAt, post.createdAt ) && Objects.equals( updatedAt, post.updatedAt );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( id, title, content, status, readingTime, createdAt, updatedAt );
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
