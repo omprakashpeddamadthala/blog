05bc50558efd50e0ae705dac1a3b1d981bc461db
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-03-22 20:05:45 +0530

    implemented exception handling delete api

--- src/main/java/com/dev/blog/service/CategoryService.java
+++ src/main/java/com/dev/blog/service/CategoryService.java
@@ -3,10 +3,13 @@ package com.dev.blog.service;
 import com.dev.blog.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
 
     List<Category> getAllCategories();
 
     Category createCategory(Category category);
+
+    void deleteCategory(UUID id);
 }
